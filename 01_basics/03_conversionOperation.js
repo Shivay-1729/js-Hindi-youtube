a61975fb40fc5300e7962661c89a3bd189d9ea59
Shivay-1729 <raghuvanshisaurabh1729@gmail.com>
2024-11-11 13:56:08 +0000

    Conversion Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = undefined
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+//console.lof(typeof valueInNumber);
+//console.lof(valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NAN but type is number and value NAN if we change this into number
+// true => 1, false => 0 if we change into number
+
+let isLoggedIn = "saurabh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+// "saurabh" =>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
